b8b8d2350123ac97b87ff61d2e696990b0d5843f
Harold Liu <harold.liu@student.unimelb.edu.au>
2022-03-08 17:01:59 +1100

    Completed task 3

--- ruby/tasks/3-file-extension.rb
+++ ruby/tasks/3-file-extension.rb
@@ -6,5 +6,10 @@
  # @returns {String} The file extension (with no period), or false if there is none.
 #
 def get_file_extension(filename)
-
+    # if no dot present then no extension name
+    if not filename.include? "."
+        return false
+    # extension name is after last dot if dot present
+    ext = filename.split(".").last
+    return ext
 end
